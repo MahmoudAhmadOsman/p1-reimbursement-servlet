506f57a0f5f785daa1bdf9ea2f3dd477d584870b
Mahmoud Osman <mahmoudosman.som@gmail.com>
2022-09-14 10:30:00 -0500

    added ReimbursementTypeServiceTest class

--- src/main/java/com/revature/northern/services/ReimbursementTypeService.java
+++ src/main/java/com/revature/northern/services/ReimbursementTypeService.java
@@ -3,6 +3,7 @@ package com.revature.northern.services;
 import com.revature.northern.daos.ReimbursementTypeDAO;
 import com.revature.northern.dtos.requests.NewRequestReimbursementType;
 import com.revature.northern.models.ReimbursementType;
+import com.revature.northern.utils.custom_exceptions.InvalidUserException;
 
 import java.util.List;
 
@@ -14,30 +15,40 @@ public class ReimbursementTypeService {
         this.reimbursementTypeDAO = reimbursementTypeDAO;
     }
 
-
     //Save ReimbursementType
     public ReimbursementType createReimbursementType(NewRequestReimbursementType request) {
         ReimbursementType reimbursementType = new ReimbursementType(request.getType_id(), request.getType());
         reimbursementTypeDAO.save(reimbursementType);
         return reimbursementType;
     }
-
-
     //Get All Reimbursement Types
     public List<ReimbursementType> getAllReimbursementType() {
         return reimbursementTypeDAO.getAll();
     }
-
     //Delete Reimbursement Type
     public void deleteReimbursementType(String id) {
         reimbursementTypeDAO.delete(id);
     }
-
-
     //update Reimbursement Type
     public void updateReimbursementType(ReimbursementType id) {
         reimbursementTypeDAO.update(id);
     }
+
+
+
+    /******* ReimbursementType Validation methods *********/
+    public boolean isValidType(String type) {
+        if (type.isEmpty()) throw new InvalidUserException("\nType is required!!");
+        if (type.length() < 4) throw new InvalidUserException("\nType must be more than 4 characters!!" );
+        return true;
+    }
+
+
+
+
+
+
+
 
 
 }
---
+++ src/test/java/com/revature/northern/services/ReimbursementTypeServiceTest.java
@@ -0,0 +1,35 @@
+package com.revature.northern.services;
+
+import com.revature.northern.daos.ReimbursementTypeDAO;
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+public class ReimbursementTypeServiceTest {
+    private ReimbursementTypeService sut;
+    private final ReimbursementTypeDAO mockReimbursementTypeDAO = mock(ReimbursementTypeDAO.class);
+
+
+    @Before
+    public void setup() {
+        sut = new ReimbursementTypeService(mockReimbursementTypeDAO);
+
+    }
+
+    @Test
+    public void test_isValid_type_givenCorrectType() {
+        //A1 - Arrange
+        String validType = "CAR RENTAL EXPENSES";
+        //A2 - Act
+        boolean flag = sut.isValidType(validType);
+        //A3 - Assert
+        Assert.assertTrue(flag);
+
+    }
+
+
+}
